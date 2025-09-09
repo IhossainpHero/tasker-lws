6c51007cbaab7c5707ff44a478fabc2ff4e1cde6
IhossainpHero <imranmollah9131@gmai;.com>
2025-09-09 15:02:30 +0600

    pc to laptop

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
